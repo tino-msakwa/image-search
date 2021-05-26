092c2812d10538d09ced360114d19bd9b6223dab
GC <gc@Tinos-MacBook-Pro.local>
2021-05-26 15:45:20 +1000

    Add search bar to project

---
+++ src/components/SearchBar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class SearchBar extends React.Component {
+
+    render() {
+        return (
+            <div>
+                <form>
+                    <div>
+                        <lable>Image Search</lable>
+                        <input type="text"></input>
+                    </div>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SearchBar;
